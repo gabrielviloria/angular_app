88156cc23db9ed63801a89f1592aec988710ce45
aether <gabe.aether@gmail.com>
2023-03-06 17:16:35 -1000

    Added 'servers' component

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  template: 
+  // templateUrl: 'servers.component.html' 
+  `
+  <app-server></app-server>
+  <app-server></app-server>
+  `,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent {
+
+}
